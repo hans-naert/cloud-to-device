0c501cc36e0cd2fbad031416c714410756b4d46f
Hans Naert <hans.naert@vives.be>
2022-12-15 13:14:41 +0100

    first revision cloud to device IoT Hub messaging

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Devices;
+using System.Text;
+
+ServiceClient serviceClient;
+string connectionString = "HostName=YourIoTHubNameHN.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=FglCwvqHJlDhCQcoQbiVsHt9fM2Ed7f9owwBFXat/1U=";
+string targetDevice = "TC74";
+
+async Task SendCloudToDeviceMessageAsync()
+{
+     var commandMessage = new
+      Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+     await serviceClient.SendAsync(targetDevice, commandMessage);
+}
+
+Console.WriteLine("Send Cloud-to-Device message\n");
+serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+Console.WriteLine("Press any key to send a C2D message.");
+Console.ReadLine();
+SendCloudToDeviceMessageAsync().Wait();
+Console.ReadLine();
